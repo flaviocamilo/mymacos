aada9214e5a4f8690b25b197dcb75dfe42879324
Flávio Camilo <flaviocamilo@gmail.com>
2016-08-10 21:35:27 -0300

    Changes system and Git editors to Atom; Avoids SC2155.

--- dotfiles/shell/vars.sh
+++ dotfiles/shell/vars.sh
@@ -5,11 +5,13 @@ export LANGUAGE='en_US.UTF-8'
 export LC_CTYPE='en_US.UTF-8'
 export LC_ALL='en_US.UTF-8'
 
-export EDITOR='/usr/bin/vim'
-export GIT_EDITOR='/usr/bin/vim'
+export EDITOR='/usr/local/bin/atom'
+export GIT_EDITOR='/usr/local/bin/atom'
 
-export HOMEBREW_HOME=$(brew --repository)
-export JAVA_HOME=$(/usr/libexec/java_home)
+HOMEBREW_HOME=$(brew --repository)
+export HOMEBREW_HOME
+JAVA_HOME=$(/usr/libexec/java_home)
+export JAVA_HOME
 
 export PATH="/usr/local/opt/coreutils/libexec/gnubin:/usr/local/opt/gnu-sed/libexec/gnubin:/usr/local/bin:/usr/libexec:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin"
 export MANPATH="/usr/local/opt/coreutils/libexec/gnuman:/usr/local/opt/gnu-sed/libexec/gnuman:$MANPATH"
